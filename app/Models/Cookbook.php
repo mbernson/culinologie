0c0c14c6c12c64a47a959df9652a998e4092548f
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-03-15 15:13:51 +0100

    Apply visibility on cookbooks

--- app/Models/Cookbook.php
+++ app/Models/Cookbook.php
@@ -1,12 +1,16 @@
 <?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasVisibilities;
 
 final class Cookbook extends Model
 {
+
+    use HasVisibilities;
 
     public function owner()
     {
         return $this->belongsTo('App\User', 'user_id');
     }
+
 }

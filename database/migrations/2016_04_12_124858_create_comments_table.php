e1bf6d4e90e6b6a48802264aaff5a84c7377d8ec
Jacek Smit <jargij@gmail.com>
2016-04-12 17:55:20 +0200

    Alles in 1 migratie

--- database/migrations/2016_04_12_124858_create_comments_table.php
+++ database/migrations/2016_04_12_124858_create_comments_table.php
@@ -22,6 +22,14 @@ class CreateCommentsTable extends Migration {
 			$table->integer('recipe_tracking_nr')->unsigned();
 			$table->timestamps();
 			$table->index(['recipe_tracking_nr','rating'], 'recipe_tracking_nr');
+			$table->foreign('recipe_tracking_nr', 'comments_ibfk_2')->references('tracking_nr')
+					->on('recipes')
+					->onUpdate('CASCADE')
+					->onDelete('CASCADE');
+			$table->foreign('user_id', 'comments_ibfk_3')->references('id')
+					->on('users')
+					->onUpdate('CASCADE')
+					->onDelete('CASCADE');
 		});
 	}
 
@@ -33,6 +41,10 @@ class CreateCommentsTable extends Migration {
 	 */
 	public function down()
 	{
+		Schema::table('comments', function(Blueprint $table) {
+			$table->dropForeign('comments_ibfk_2');
+			$table->dropForeign('comments_ibfk_3');
+		});
 		Schema::drop('comments');
 	}
 
